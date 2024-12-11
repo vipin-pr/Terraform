f8892571fc007188b4807c79f2d27dfaf0b2e2a8
vipin-pr <vipinrajan77@gmail.com>
2024-12-12 01:33:13 +0530

    aws_vpc module added

---
+++ AWS/AWS_Infra/modules/aws_vpc_module/aws_vpc.tf
@@ -0,0 +1,85 @@
+#aws vpc resource
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = var.instance_tenancy
+
+  tags = {
+    Name = "vpc-${terraform.workspace}"
+  }
+}
+
+#aws internet gateway resource
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "gateway-${terraform.workspace}"
+  }
+}
+
+
+#aws private subnet resource
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnet_cidr_block
+  map_public_ip_on_launch = false # Private subnet
+
+  tags = {
+    Name = "private-subnet-${terraform.workspace}"
+  }
+}
+
+#aws public subnet resource
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_cidr_block
+
+  tags = {
+    Name = "public-subnet-${terraform.workspace}"
+}
+}
+
+#aws public route table resource
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  route {
+      cidr_block = var.vpc_cidr_block
+      gateway_id = "local"
+    }
+  tags = {
+    Name = "public-subnet-route-table-${terraform.workspace}"
+  }
+}
+
+#aws private subnet resource
+resource "aws_route_table" "private_route_table" {
+  vpc_id     = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.vpc_cidr_block
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "private-subnet-route-table-${terraform.workspace}"
+  }
+}
+
+
+resource "aws_route_table_association" "private_route_table_association" {
+ subnet_id = aws_subnet.private_subnet.id
+ route_table_id = aws_route_table.private_route_table.id
+}
+
+
+resource "aws_route_table_association" "public_route_table_association" {
+ subnet_id = aws_subnet.public_subnet.id
+ route_table_id = aws_route_table.public_route_table.id
+}
+
---
+++ AWS/AWS_Infra/modules/aws_vpc_module/variable.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  default = "default"
+}
+
+variable "private_subnet_cidr_block" {
+  default = "10.0.10.0/24"
+}
+
+variable "public_subnet_cidr_block" {
+  default = "10.0.1.0/24"
+}
+
