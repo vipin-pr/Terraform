8ef49dd1aebd17bc0d913156329c040e90267d6c
vipin-pr <vipinrajan77@gmail.com>
2024-12-20 23:17:22 +0530

    AWS-vpc done

---
+++ AWS/AWS_Infra/main.tf
@@ -0,0 +1,3 @@
+module "aws_vpc" {
+  source = "./modules/aws_vpc_module"
+}
--- AWS/AWS_Infra/modules/aws_vpc_module/aws_vpc.tf
+++ AWS/AWS_Infra/modules/aws_vpc_module/aws_vpc.tf
@@ -71,15 +71,24 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-
+# route table association for private subnet
 resource "aws_route_table_association" "private_route_table_association" {
  subnet_id = aws_subnet.private_subnet.id
  route_table_id = aws_route_table.private_route_table.id
 }
 
-
+# route table association for public subnet
 resource "aws_route_table_association" "public_route_table_association" {
  subnet_id = aws_subnet.public_subnet.id
  route_table_id = aws_route_table.public_route_table.id
 }
 
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "NAT-gateway-${terraform.workspace}"
+  }
+
+  depends_on = [aws_internet_gateway.gateway]
+}
